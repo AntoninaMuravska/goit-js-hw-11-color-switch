ecfcca78960994a5fdd64bf385688c312339bdeb
Antonina Muravska <at.tonya@gmail.com>
2020-12-21 11:45:41 +0200

    add disabled attribute

--- src/index.js
+++ src/index.js
@@ -10,10 +10,10 @@ const colors = [
 ];
 
 const refs = {
-    startBtn: document.querySelector('button[data-action="start"]'),
-    stopBtn: document.querySelector('button[data-action="stop"]'),
-    bodyRef: document.querySelector('body'),
-}
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+  bodyRef: document.querySelector('body'),
+};
 
 let intervalId;
 let isActive = false;
@@ -22,27 +22,28 @@ refs.startBtn.addEventListener('click', startChangeColor);
 refs.stopBtn.addEventListener('click', stopChangeColor);
 
 // Для генерации случайного числа (индекс элемента массива цветов), используй функцию randomIntegerFromInterval.
- const randomIntegerFromInterval = (min, max) => {
-        return Math.floor(Math.random() * (max - min + 1) + min);
- };
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
 function startChangeColor() {
-    if (isActive) {
-      return;
-    };
+  if (isActive) {
+    return;
+  }
 
-    intervalId = setInterval(onChangingColor, 1000);
-    isActive = true;
-};
-
-function stopChangeColor() {
-    clearInterval(intervalId);
-    isActive = false;
-};
-
-function onChangingColor() {
-    let randomIndexOfElement = randomIntegerFromInterval(0, colors.length - 1);
-    let colorOfBody = colors[randomIndexOfElement];
-    refs.bodyRef.style.backgroundColor = colorOfBody;
+  intervalId = setInterval(onChangingColor, 1000);
+  isActive = true;
+  refs.startBtn.disabled = true;
 }
 
+function stopChangeColor() {
+  clearInterval(intervalId);
+  isActive = false;
+  refs.startBtn.disabled = false;
+}
+
+function onChangingColor() {
+  let randomIndexOfElement = randomIntegerFromInterval(0, colors.length - 1);
+  let colorOfBody = colors[randomIndexOfElement];
+  refs.bodyRef.style.backgroundColor = colorOfBody;
+}
